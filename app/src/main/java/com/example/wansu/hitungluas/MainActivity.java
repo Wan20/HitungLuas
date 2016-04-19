d5ad270e25c21d976d9f615e58b95dd2f70c8eb8
Wan20 <wandy20number@gmail.com>
2016-04-19 20:30:47 +0700

    Program

---
+++ app/src/main/java/com/example/wansu/hitungluas/MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.wansu.hitungluas;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText edtPanjang, edtLebar;
+    private TextView txtLuas;
+    private Button btnHitung;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        getSupportActionBar().setTitle("Hitung Luas Persegi Panjang");
+        edtPanjang = (EditText) findViewById(R.id.edt_panjang);
+        edtLebar = (EditText) findViewById(R.id.edt_lebar);
+        btnHitung = (Button) findViewById(R.id.btn_hitung);
+        txtLuas = (TextView) findViewById(R.id.txt_luas);
+        btnHitung.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String panjang = edtPanjang.getText().toString().trim();
+                String lebar = edtLebar.getText().toString().trim();
+                try {
+                    if (panjang.equals("")) {
+                        edtPanjang.setError("Isi Kolom Panjang");
+                    }
+                    if (lebar.equals("")) {
+                        edtLebar.setError("Isi Kolom Lebar");
+                    }
+                    double p = Double.parseDouble(panjang);
+                    double l = Double.parseDouble(lebar);
+                    double luas = p * l;
+                    txtLuas.setText("Luas : " + luas);
+                } catch (Exception e) {
+                    txtLuas.setText("Anda belum mengisi panjang dan lebar");
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
